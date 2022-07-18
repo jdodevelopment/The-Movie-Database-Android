3022a9517e076380b633a3c070280cc91999be2b
Juan Daniel Ornella <juan.daniel.sp@gmail.com>
2022-07-18 00:13:45 -0300

    Setup Splash Screen

--- app/src/main/java/ar/com/jdodevelopment/tmdb/MainActivity.kt
+++ app/src/main/java/ar/com/jdodevelopment/tmdb/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ar.com.jdodevelopment.tmdb.ui.theme.TmdbApplicationTheme
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             TmdbApplicationTheme {
                 Text("Hello")
